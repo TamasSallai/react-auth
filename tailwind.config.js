2ea65affa8bb7947a77fc55ffec8094b15ebab58
Tamas Sallai <tamas.sallai.hu@gmail.com>
2024-03-22 21:56:04 +0100

    created plugin for aria-invalid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  theme: {},
+  plugins: [
+    plugin(function ({ addVariant, e }) {
+      addVariant('aria-invalid', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.${e(
+            `aria-invalid${separator}${className}`
+          )}[aria-invalid="true"]`
+        })
+      })
+    }),
+  ],
 }
