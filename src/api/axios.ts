432bf217f41fc97b8e3d3b992c1bf538e9d180fd
Tamas Sallai <tamas.sallai.hu@gmail.com>
2024-03-23 18:32:45 +0100

    added backend url as env var

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
 export default axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: `${BACKEND_URL}/api`,
   withCredentials: true,
 })
