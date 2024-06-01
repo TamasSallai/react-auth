b01cdaf682e587226cfd172393935a57ef669e21
Tamas Sallai <tamas.sallai.hu@gmail.com>
2024-06-01 16:52:08 +0200

    created hook for user state

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,13 @@
+import { useQuery } from '@tanstack/react-query'
+import { profile } from '../api/auth'
+import { User } from '../types'
+
+const useUser = () => {
+  return useQuery<User | null>({
+    queryKey: ['user'],
+    queryFn: profile,
+    retry: false,
+  })
+}
+
+export default useUser
