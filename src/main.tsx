f0574b3e4d02a77d601b3443ccc54e851e0f4a16
Tamas Sallai <tamas.sallai.hu@gmail.com>
2024-06-01 16:50:42 +0200

    added react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/auth/state.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 import './index.css'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
+      <QueryClientProvider client={queryClient}>
         <App />
-      </AuthProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
